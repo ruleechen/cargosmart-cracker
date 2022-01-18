c334a69f4032db69c36f64df0065197a627f2174
Rulee Chen <rulee@live.cn>
2022-01-18 10:46:43 +0800

    feat(crack): brand new implementation

--- src/crack.js
+++ src/crack.js
@@ -19,6 +19,20 @@ const decodeBase64DataUri = (dataString) => {
   };
 };
 
+const toRGB = (color) => {
+  // return Jimp.intToRGBA(color);
+  return {
+    r: (color >> 24) & 255,
+    g: (color >> 16) & 255,
+    b: (color >> 8) & 255,
+  };
+};
+
+const round = (number, decimals) => {
+  const scale = Math.pow(10, decimals);
+  return Math.round(number * scale) / scale;
+};
+
 const crack = async ({ groundDataString, brickDataString }) => {
   let groundBuf;
   try {
@@ -34,75 +48,89 @@ const crack = async ({ groundDataString, brickDataString }) => {
     brickBuf = Buffer.from(brickDataString, "base64");
   }
 
-  const ground = await Jimp.read(groundBuf);
+  // border pixels of brick
   const brick = await Jimp.read(brickBuf);
-
-  const groundWidth = ground.getWidth();
-  const groundHeight = ground.getHeight();
   const brickWidth = brick.getWidth();
   const brickHeight = brick.getHeight();
-
-  const border = [];
-  let brickMinY = Number.MAX_VALUE;
-
+  const borderPixels = [];
   for (let brickX = 0; brickX < brickWidth; brickX++) {
-    const yPoints = [];
+    const yPixels = [];
     for (let brickY = 0; brickY < brickHeight; brickY++) {
       const color = brick.getPixelColor(brickX, brickY);
       if (color) {
-        yPoints.push({
+        yPixels.push({
           brickY,
-          color,
+          color: toRGB(color),
         });
       }
     }
-    if (yPoints.length) {
-      const pair = {
-        brickX,
-        topY: yPoints[0].brickY,
-        topYColor: yPoints[0].color,
-        bottomY: yPoints[yPoints.length - 1].brickY,
-        bottomYColor: yPoints[yPoints.length - 1].color,
-      };
-      if (pair.topY < brickMinY) {
-        brickMinY = pair.topY;
-      }
-      border.push(pair);
+    if (yPixels.length > 0) {
+      const pixel = yPixels[0];
+      borderPixels.push({
+        x: brickX,
+        y: pixel.brickY,
+        color: pixel.color,
+      });
+    }
+    if (yPixels.length > 1) {
+      const pixel = yPixels[yPixels.length - 1];
+      borderPixels.push({
+        x: brickX,
+        y: pixel.brickY,
+        color: pixel.color,
+      });
+    }
+  }
+  const minBrickX = borderPixels.reduce((prev, curr) => {
+    return prev.x < curr.x ? prev : curr;
+  }).x;
+  const minBrickY = borderPixels.reduce((prev, curr) => {
+    return prev.y < curr.y ? prev : curr;
+  }).y;
+
+  // all pixels of ground
+  const ground = await Jimp.read(groundBuf);
+  const groundWidth = ground.getWidth();
+  const groundHeight = ground.getHeight();
+  const groundPixels = {};
+  for (let groundX = 0; groundX < groundWidth; groundX++) {
+    groundPixels[groundX] = {};
+    for (let groundY = 0; groundY < groundHeight; groundY++) {
+      const color = ground.getPixelColor(groundX, groundY);
+      groundPixels[groundX][groundY] = toRGB(color);
     }
   }
 
-  console.log({
-    brickMinY,
-    groundWidth,
-    groundHeight,
-    brickWidth,
-    brickHeight,
+  // do compare
+  const compares = [];
+  for (let groundX = 0; groundX < groundWidth - brickWidth; groundX++) {
+    const diffs = [];
+    borderPixels.forEach(({ x, y, color }) => {
+      const groundColor = groundPixels[groundX + x][y];
+      const redDiff = color.r - groundColor.r;
+      const greenDiff = color.g - groundColor.g;
+      const blueDiff = color.b - groundColor.b;
+      diffs.push((redDiff + greenDiff + blueDiff) / 3);
+    });
+    const total = diffs.reduce((prev, curr) => prev + curr, 0);
+    const average = total / diffs.length;
+    const confidence = 1 - average / 255;
+    compares.push({
+      groundX,
+      confidence,
+    });
+  }
+
+  // the best confidence
+  const best = compares.reduce((prev, curr) => {
+    return prev.confidence > curr.confidence ? prev : curr;
   });
 
-  const clonedBorder = JSON.parse(JSON.stringify(border));
-  let pairIndex = 0;
-  for (let groundX = 0; groundX < groundWidth; groundX++) {
-    const pair = clonedBorder[pairIndex];
-    if (pair) {
-      const topColor = ground.getPixelColor(groundX, pair.topY);
-      const bottomColor = ground.getPixelColor(groundX, pair.bottomY);
-      if (topColor === bottomColor) {
-        pair.groundX = groundX;
-        pair.groundColor = topColor;
-        pairIndex++;
-      } else if (pairIndex !== 0) {
-        pairIndex = 0;
-      }
-    }
-  }
-
-  const firstMatchedPair = clonedBorder.find((x) => x.groundColor);
-
+  // done
   return {
-    x: firstMatchedPair
-      ? firstMatchedPair.groundX - firstMatchedPair.brickX
-      : -1,
-    y: firstMatchedPair ? brickMinY : -1,
+    confidence: round(best.confidence, 4),
+    x: best.groundX + minBrickX,
+    y: minBrickY,
   };
 };
 
